61145bd24ac04168c7bd46fe4db11db97006b074
Lucas Verdan Correa <154383154+imlverdan@users.noreply.github.com>
2024-02-21 00:20:35 -0300

    Add files via upload

--- funcionario/Program.cs
+++ funcionario/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace funcionario
+﻿using System;
+
+public class Funcionario
 {
-    internal class Program
+    
+    public string Nome { get; set; }
+    public decimal Salario { get; set; }
+    public string Cargo { get; set; }
+
+    
+    public Funcionario(string nome, decimal salario, string cargo)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Nome = nome;
+        Salario = salario;
+        Cargo = cargo;
+    }
+
+    
+    public decimal SalarioLiquido(decimal impostos, decimal beneficios)
+    {
+        
+        decimal salarioLiquido = Salario - impostos + beneficios;
+        return salarioLiquido;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Funcionario funcionario = new Funcionario("Lucas", 1412, "Gerente");
+
+        
+        decimal salarioLiquido = funcionario.SalarioLiquido(400, 1412);
+
+        
+        Console.WriteLine($"O salário líquido de {funcionario.Nome} como {funcionario.Cargo} é: R${salarioLiquido}");
     }
 }
